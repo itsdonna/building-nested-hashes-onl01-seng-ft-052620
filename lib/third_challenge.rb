24717e138dc4931a2c9d48129a7e7e7de9dc765e
Donna Torres <todonnatorres@icloud.com>
2020-06-06 17:07:16 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,7 @@ def third_challenge
       :hero_friends => [{ :name => "Benvolio", :age => "17", :attitude => "worried"}, { :name => "Mercutio", :age => "18", :attitude => "hot-headed"}]
    }, 
    :capulet => {
-      :patriarch => {
+      :patriarch => {:name => "Lord Capulet", :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
