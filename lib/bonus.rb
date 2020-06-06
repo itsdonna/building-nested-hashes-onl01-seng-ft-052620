65324c11f80f547b42f472b831852679beb4bd30
Donna Torres <todonnatorres@icloud.com>
2020-06-06 17:21:38 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -25,6 +25,6 @@ epic_tragedy[:capulet][:heroine][:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  tragedy hash
+  
   epic_tragedy
 end
